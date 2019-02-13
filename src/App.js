576d873fb6238b1594f5fe2dd083162bf438a7fb
SvetlanaZhak <lana.zhak@gmail.com>
2019-02-13 23:19:21 +0200

    Exercise 7
    Split todo list application to separate
    components: App and TodoTable

--- src/App.js
+++ src/App.js
@@ -1,46 +1,47 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TodoTable from './TodoTable';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {description: '', todos: [], date: ''}
- 
+    this.state = { description: '', todos: [], date: '' }
+
   }
 
   descriptionChanged = (event) => {
-    this.setState({description: event.target.value});
+    this.setState({ description: event.target.value });
   }
   dateChanged = (event) => {
-    this.setState({date: event.target.value});
+    this.setState({ date: event.target.value });
   }
 
   addTodo = (event) => {
     event.preventDefault();
     this.setState({
-      todos: [...this.state.todos, {description: this.state.description, date: this.state.date}]
+      todos: [...this.state.todos, { description: this.state.description, date: this.state.date }]
     });
   }
- 
+
   deleteTodo = (index) => {
-      this.setState({
-        todos: this.state.todos.filter((todo, i) => i !== index)
-      });
+    this.setState({
+      todos: this.state.todos.filter((todo, i) => i !== index)
+    });
   }
- 
-  
+
+
   renderRow = (item, index) => {
-      return  (
-          <tr>
-            <td>{item.description}</td>
-            <td>{item.date}</td>
-          <td><input type="button" onClick={() => this.deleteTodo(index)} value="Delete"/></td>
-          </tr>
-        
-      )
+    return (
+      <tr>
+        <td>{item.description}</td>
+        <td>{item.date}</td>
+        <td><input type="button" onClick={() => this.deleteTodo(index)} value="Delete" /></td>
+      </tr>
+
+    )
   }
-  
+
   render() {
     return (
       <div className="App">
@@ -50,36 +51,24 @@ class App extends Component {
         <div>
           <form onSubmit={this.addTodo}>
             <div className="Div-inputs">
-                <div>
-                    <div>Date:</div>
-                    <input type="text" onChange={this.descriptionChanged} value={this.state.description}/>
-                </div>
-                <div>
-                    <div>Description:</div>
-                    <input type="text" onChange={this.dateChanged} value= {this.state.date}/>
-                </div>
-                <input type="submit" value="Add"/>
+              <div>
+                <div>Date:</div>
+                <input type="text" onChange={this.descriptionChanged} value={this.state.description} />
+              </div>
+              <div>
+                <div>Description:</div>
+                <input type="text" onChange={this.dateChanged} value={this.state.date} />
+              </div>
+              <input type="submit" value="Add" />
             </div>
           </form>
-     
+
         </div>
-        <div className="Table">
-          <table >
-        <tbody>
-            <tr>
-                <th>Date</th>
-                <th>Description</th>
-        
-            </tr>
-            {this.state.todos.map((item, index) => this.renderRow(item, index))}
-        </tbody>
-        
-          </table>
-        </div>          
-      </div>    
+        <TodoTable todos={this.state.todos} deleteTodo={this.deleteTodo} />
+      </div>
     );
   }
-  
+
 }
 
 export default App;
--- src/TodoTable.js
+++ src/TodoTable.js
@@ -1,8 +1,48 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Table = styled.div`
+display: flex;
+flex-direction: row;
+align-items: center;
+justify-content: center;
+margin: auto;
+width: 100%;
+`;
+
+const HeaderCell = styled.th`
+border: 5px solid #ddd;
+background-color: lightgreen;
+padding: 20px;
+`
+
+const Cell = styled.td`
+border: 5px solid #ddd;
+padding: 20px;
+`
+const DelBut = styled.td`
+
+`
+
+
 const TodoTable = (props) => {
-return (
-<div>
-</div>
-);
+
+    return (
+        <Table>
+            <table>
+                <tbody>
+                    <tr><HeaderCell>Date</HeaderCell><HeaderCell>Description</HeaderCell></tr>
+                    {props.todos.map((item, index) =>
+                        <tr key={index}>
+                            <Cell>{item.date}</Cell>
+                            <Cell>{item.description}</Cell>
+                            <DelBut> < input type="button" onClick={() => props.deleteTodo(index)} value="Delete" /></DelBut>
+                        </tr>
+                    )}
+
+                </tbody>
+            </table>
+        </Table >
+    );
 }
 export default TodoTable;
